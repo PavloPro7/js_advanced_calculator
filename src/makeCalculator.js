d4e89f1f42bd5e95b5f7520f027d6ab744862107
PavloPro7 <03ppcv@gmail.com>
2026-09-22 22:48:50 +0200

    add Advanced Calculator

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -3,8 +3,40 @@
 /**
  * @return {object}
  */
-function makeCalculator() {
-  // write code here
-}
+const makeCalculator = () => {
+  const calculator = {
+    result: 0,
+
+    operate(operation, value) {
+      operation(this, value);
+
+      return this;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    add(object, value) {
+      object.result = object.result + value;
+    },
+
+    subtract(object, value) {
+      object.result = object.result - value;
+    },
+
+    divide(object, value) {
+      object.result = object.result / value;
+    },
+
+    multiply(object, value) {
+      object.result = object.result * value;
+    },
+  };
+
+  return calculator;
+};
 
 module.exports = makeCalculator;
